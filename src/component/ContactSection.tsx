b4bdf8b66d9a8ab2aac8be98fbb67b55e0f409ad
Tiara Pujilestari <tiarapujilestari009@gmail.com>
2026-06-13 13:36:33 +0700

    fix : contact section

--- src/component/ContactSection.tsx
+++ src/component/ContactSection.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ContactSection() {
   return (
     <div
@@ -8,7 +6,7 @@ export default function ContactSection() {
     >
       <div className="w-full text-center pt-8 pb-12 z-10"></div>
 
-      {/*BACKGROUND*/}
+      {/* BACKGROUND */}
       <div className="absolute top-1/2 right-0 w-96 h-96 bg-cyan-500/10 rounded-full blur-[120px] pointer-events-none" />
       <div className="absolute bottom-0 left-0 w-96 h-96 bg-indigo-500/5 rounded-full blur-[120px] pointer-events-none" />
 
@@ -29,7 +27,7 @@ export default function ContactSection() {
         <form className="space-y-6" onSubmit={(e) => e.preventDefault()}>
           {/* EMAIL ADDRESS */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {/* FULL NAME*/}
+            {/* FULL NAME */}
             <div className="flex flex-col space-y-2">
               <label className="text-xs font-mono tracking-widest text-slate-400 uppercase">
                 Full Name
@@ -41,7 +39,7 @@ export default function ContactSection() {
               />
             </div>
 
-            {/* EMAIL ADRESS */}
+            {/* EMAIL ADDRESS */}
             <div className="flex flex-col space-y-2">
               <label className="text-xs font-mono tracking-widest text-slate-400 uppercase">
                 Email Address
